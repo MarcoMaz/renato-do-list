b30fe6ce07ba7883dcf100a0f57966254c455caa
Marco Mazzai <mazzai.marco@gmail.com>
2021-08-22 21:39:25 +0200

    feat - add basic structure

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,13 @@
 import './App.scss';
 
 // Router
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 // Redux
 import { Provider } from 'react-redux';
+import { useState } from 'react';
 import { useAppSelector, useAppDispatch } from './state/hooks';
-import { decrement, increment } from './state/counterSlice';
+import { addTodo, toggleTodo, removeTodo } from './state/counterSlice';
 import store from './state/store';
 
 // Views
@@ -17,45 +18,141 @@ import { Home } from './views/Home';
 import { View } from './views/View';
 
 // Components
-import { Button } from './components/Button';
 
 function App() {
-  const count = useAppSelector((state) => state.counter.value);
+  const heroes = useAppSelector((state) => state.counter);
+  const heroes2 = useAppSelector((state) =>
+    state.counter.map((x) => x.isCompleted),
+  );
+
+  const thingsTotal = heroes.length;
+  const thingsDone = heroes2.filter(Boolean).length;
+  const thingsTodo = thingsTotal - thingsDone;
   const dispatch = useAppDispatch();
+
+  const [value, setValue] = useState('');
+  const [velox, setVelox] = useState(0);
+  const [urg, setUrg] = useState(0);
+  const [funny, setFunny] = useState(0);
+
+  const [showTodo, setShowTodo] = useState(false);
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const onSubmit = (event: any) => {
+    event.preventDefault();
+    dispatch(
+      addTodo({
+        label: value,
+        speed: velox,
+        urgency: urg,
+        fun: funny,
+      }),
+    );
+    setValue('');
+    setShowTodo(false);
+  };
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
 
   return (
     <Router>
       <Provider store={store}>
         <div className="App">
-          <div>
-            <button
-              type="button"
-              aria-label="Increment value"
-              onClick={() => dispatch(increment())}
-            >
-              Increment
-            </button>
-            <button
-              type="button"
-              aria-label="Decrement value"
-              onClick={() => dispatch(decrement())}
-            >
-              Decrement
-            </button>
-            <h1>{count}</h1>
-          </div>
-          <hr />
-          Views
+          <h2>
+            Cose da fare <span>{`(${thingsTodo}/${thingsTotal})`}</span>
+          </h2>
           <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/view">View</Link>
-            </li>
+            {heroes
+              .filter((x) => !x.isCompleted)
+              .sort((a, b) => a.total - b.total)
+              .map((hero) => (
+                <li key={hero.id}>
+                  <input
+                    value={hero.id}
+                    onChange={() => dispatch(toggleTodo(hero.id))}
+                    type="checkbox"
+                    checked={hero.isCompleted}
+                  />
+                  {hero.label}
+                  <button
+                    onClick={() => dispatch(removeTodo(hero.id))}
+                    type="button"
+                  >
+                    X
+                  </button>
+                  <br />
+                  <small> speed: {hero.speed} </small>
+                  <small> urgency: {hero.urgency} </small>
+                  <small> funny: {hero.fun} --- </small>
+                  <strong> total: {hero.total} </strong>
+                </li>
+              ))}
           </ul>
-          <hr />
-          <Button label="button" />
+          <h2>
+            Cose fatte <span>{`(${thingsDone}/${thingsTotal})`}</span>
+          </h2>
+          <ul>
+            {heroes
+              .filter((x) => x.isCompleted)
+              .map((x, index) => (
+                <li key={index}>{x.label}</li>
+              ))}
+          </ul>
+          <button type="button" onClick={() => setShowTodo(true)}>
+            ADD TODO
+          </button>
+          <br />
+          <br />
+          {showTodo && (
+            <form onSubmit={onSubmit}>
+              <input
+                type="text"
+                placeholder="Add todo..."
+                value={value}
+                onChange={(event) => setValue(event.target.value)}
+              />
+              <button type="submit" className="btn btn-primary mb-2">
+                Submit
+              </button>
+              <br />
+              <label htmlFor="lunghezza">
+                <select
+                  name="lunghezza"
+                  id="lunghezza"
+                  onChange={(e) => setVelox(Number(e.target.value))}
+                >
+                  <option value={1}>lento</option>
+                  <option value={2}>medio</option>
+                  <option value={3}>veloce</option>
+                </select>
+              </label>
+              <br />
+              <label htmlFor="urgenza">
+                <select
+                  name="urgenza"
+                  id="urgenza"
+                  onChange={(e) => setUrg(Number(e.target.value))}
+                >
+                  <option value={1}>non-urgente</option>
+                  <option value={100}>urgente</option>
+                </select>
+              </label>
+              <br />
+              <label htmlFor="fun">
+                <select
+                  name="fun"
+                  id="fun"
+                  onChange={(e) => setFunny(Number(e.target.value))}
+                >
+                  <option value={1}>funny</option>
+                  <option value={3}>NOT funny</option>
+                </select>
+              </label>
+              <button type="button" onClick={() => setShowTodo(false)}>
+                Click me if you change your mind
+              </button>
+            </form>
+          )}
         </div>
       </Provider>
       <Switch>
--- src/state/counterSlice.ts
+++ src/state/counterSlice.ts
@@ -1,37 +1,74 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 /* eslint-disable no-param-reassign */
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
 import type { RootState } from './store';
 
 // Define a type for the slice state
 interface CounterState {
-  value: number;
+  id: number;
+  label: string;
+  isCompleted: boolean;
+  speed: number;
+  urgency: number;
+  fun: number;
+  total: number;
 }
 
 // Define the initial state using that type
-const initialState: CounterState = {
-  value: 0,
-};
+const initialState: CounterState[] = [
+  {
+    id: 0,
+    label: 'batman',
+    isCompleted: false,
+    speed: 3,
+    urgency: 100,
+    fun: 3,
+    total: 106,
+  },
+  {
+    id: 1,
+    label: 'robin',
+    isCompleted: false,
+    speed: 2,
+    urgency: 1,
+    fun: 1,
+    total: 4,
+  },
+];
 
 export const counterSlice = createSlice({
   name: 'counter',
   initialState,
   reducers: {
-    increment: (state) => {
-      state.value += 1;
+    addTodo: (state, action) => {
+      const newTodo = {
+        id: Date.now(),
+        label: action.payload.label,
+        isCompleted: false,
+        speed: action.payload.speed,
+        urgency: action.payload.urgency,
+        fun: action.payload.fun,
+        total:
+          action.payload.speed + action.payload.urgency + action.payload.fun,
+      };
+      state.push(newTodo);
     },
-    decrement: (state) => {
-      state.value -= 1;
+    toggleTodo: (state, action) => {
+      const todoss = state.find((todo) => todo.id === action.payload);
+      if (todoss) {
+        todoss.isCompleted = !todoss.isCompleted;
+      }
     },
-    incrementByAmount: (state, action: PayloadAction<number>) => {
-      state.value += action.payload;
+    removeTodo: (state, action) => {
+      const yeah = state.filter((todo) => todo.id !== action.payload);
+      return yeah;
     },
   },
 });
 
 // Action creators are generated for each case reducer function
-export const { increment, decrement, incrementByAmount } = counterSlice.actions;
+export const { addTodo, toggleTodo, removeTodo } = counterSlice.actions;
 
-export const selectCount = (state: RootState) => state.counter.value;
+export const selectCount = (state: RootState) => state.counter;
 
 export default counterSlice.reducer;
