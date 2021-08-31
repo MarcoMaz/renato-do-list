b4aa3d985be989f88c386e2b496c8cee79c16bf0
Marco Mazzai <mazzai.marco@gmail.com>
2021-08-31 22:22:41 +0200

    fix naming

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,6 @@
 // Styling
 import './App.scss';
 
-// Router
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-
 // Redux
 import { Provider } from 'react-redux';
 import { useState } from 'react';
@@ -13,157 +10,134 @@ import { useAppSelector, useAppDispatch } from './state/hooks';
 import { addTodo, toggleTodo, removeTodo } from './state/counterSlice';
 import store from './state/store';
 
-// Views
-import { Home } from './views/Home';
-import { View } from './views/View';
-
-// Components
-
 function App() {
-  const heroes = useAppSelector((state) => state.counter);
-  const heroes2 = useAppSelector((state) =>
-    state.counter.map((x) => x.isCompleted),
+  const todos = useAppSelector((state) => state.counter);
+  const completedTodos = useAppSelector((state) =>
+    state.counter.map((todo) => todo.isCompleted),
   );
 
-  const thingsTotal = heroes.length;
-  const thingsDone = heroes2.filter(Boolean).length;
+  const thingsTotal = todos.length;
+  const thingsDone = completedTodos.filter(Boolean).length;
   const thingsTodo = thingsTotal - thingsDone;
   const dispatch = useAppDispatch();
 
-  const [value, setValue] = useState('');
-  const [velox, setVelox] = useState(0);
-  const [urg, setUrg] = useState(0);
-  const [funny, setFunny] = useState(0);
+  const [label, setLabel] = useState('');
+  const [speed, setSpeed] = useState(0);
+  const [urgency, setUrgency] = useState(0);
+  const [fun, setFun] = useState(0);
 
-  const [showTodo, setShowTodo] = useState(false);
+  const [showCreateNew, setShowCreateNew] = useState(false);
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const onSubmit = (event: any) => {
     event.preventDefault();
     dispatch(
       addTodo({
-        label: value,
-        speed: velox,
-        urgency: urg,
-        fun: funny,
+        label,
+        speed,
+        urgency,
+        fun,
       }),
     );
-    setValue('');
-    setShowTodo(false);
+    setLabel('');
+    setShowCreateNew(false);
   };
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
 
   return (
-    <Router>
-      <Provider store={store}>
-        <div className="App">
-          <h2>
-            Cose da fare <span>{`(${thingsTodo}/${thingsTotal})`}</span>
-          </h2>
-          <ul>
-            {heroes
-              .filter((x) => !x.isCompleted)
-              .sort((a, b) => a.total - b.total)
-              .map((hero) => (
-                <li key={hero.id}>
-                  <input
-                    value={hero.id}
-                    onChange={() => dispatch(toggleTodo(hero.id))}
-                    type="checkbox"
-                    checked={hero.isCompleted}
-                  />
-                  {hero.label}
-                  <button
-                    onClick={() => dispatch(removeTodo(hero.id))}
-                    type="button"
-                  >
-                    X
-                  </button>
-                  <br />
-                  <small> speed: {hero.speed} </small>
-                  <small> urgency: {hero.urgency} </small>
-                  <small> funny: {hero.fun} --- </small>
-                  <strong> total: {hero.total} </strong>
-                </li>
-              ))}
-          </ul>
-          <h2>
-            Cose fatte <span>{`(${thingsDone}/${thingsTotal})`}</span>
-          </h2>
-          <ul>
-            {heroes
-              .filter((x) => x.isCompleted)
-              .map((x, index) => (
-                <li key={index}>{x.label}</li>
-              ))}
-          </ul>
-          <button type="button" onClick={() => setShowTodo(true)}>
-            ADD TODO
-          </button>
-          <br />
-          <br />
-          {showTodo && (
-            <form onSubmit={onSubmit}>
-              <input
-                type="text"
-                placeholder="Add todo..."
-                value={value}
-                onChange={(event) => setValue(event.target.value)}
-              />
-              <button type="submit" className="btn btn-primary mb-2">
-                Submit
-              </button>
-              <br />
-              <label htmlFor="lunghezza">
-                <select
-                  name="lunghezza"
-                  id="lunghezza"
-                  onChange={(e) => setVelox(Number(e.target.value))}
-                >
-                  <option value={1}>lento</option>
-                  <option value={2}>medio</option>
-                  <option value={3}>veloce</option>
-                </select>
-              </label>
-              <br />
-              <label htmlFor="urgenza">
-                <select
-                  name="urgenza"
-                  id="urgenza"
-                  onChange={(e) => setUrg(Number(e.target.value))}
-                >
-                  <option value={1}>non-urgente</option>
-                  <option value={100}>urgente</option>
-                </select>
-              </label>
-              <br />
-              <label htmlFor="fun">
-                <select
-                  name="fun"
-                  id="fun"
-                  onChange={(e) => setFunny(Number(e.target.value))}
-                >
-                  <option value={1}>funny</option>
-                  <option value={3}>NOT funny</option>
-                </select>
-              </label>
-              <button type="button" onClick={() => setShowTodo(false)}>
-                Click me if you change your mind
-              </button>
-            </form>
-          )}
-        </div>
-      </Provider>
-      <Switch>
-        <Route exact path="/">
-          <Home label="home" />
-        </Route>
-        <Route path="/view">
-          <View label="view" />
-        </Route>
-      </Switch>
-    </Router>
+    <Provider store={store}>
+      <div className="App">
+        <h2>
+          Things to do <span>{`(${thingsTodo}/${thingsTotal})`}</span>
+        </h2>
+        <ul>
+          {todos
+            .filter((todo) => !todo.isCompleted)
+            .sort((a, b) => a.total - b.total)
+            .map(({ id, isCompleted, total }) => (
+              <li key={id}>
+                <input
+                  value={id}
+                  onChange={() => dispatch(toggleTodo(id))}
+                  type="checkbox"
+                  checked={isCompleted}
+                />
+                {label}
+                <button onClick={() => dispatch(removeTodo(id))} type="button">
+                  X
+                </button>
+                <br />
+                <small> speed: {speed} </small>
+                <small> urgency: {urgency} </small>
+                <small> funny: {fun} --- </small>
+                <strong> total: {total} </strong>
+              </li>
+            ))}
+        </ul>
+        <h2>
+          Things done <span>{`(${thingsDone}/${thingsTotal})`}</span>
+        </h2>
+        <ul>
+          {todos
+            .filter((todo) => todo.isCompleted)
+            .map((todo, index) => (
+              <li key={index}>{todo.label}</li>
+            ))}
+        </ul>
+        <button type="button" onClick={() => setShowCreateNew(true)}>
+          ADD TODO
+        </button>
+        {showCreateNew && (
+          <form onSubmit={onSubmit}>
+            <input
+              type="text"
+              placeholder="Add todo..."
+              value={label}
+              onChange={(event) => setLabel(event.target.value)}
+            />
+            <button type="submit">Submit</button>
+            <br />
+            <label htmlFor="speed">
+              <select
+                name="speed"
+                id="speed"
+                onChange={(e) => setSpeed(Number(e.target.value))}
+              >
+                <option value={1}>slow</option>
+                <option value={2}>medium</option>
+                <option value={3}>fast</option>
+              </select>
+            </label>
+            <br />
+            <label htmlFor="urgency">
+              <select
+                name="urgency"
+                id="urgency"
+                onChange={(e) => setUrgency(Number(e.target.value))}
+              >
+                <option value={1}>non-urgent</option>
+                <option value={100}>urgent</option>
+              </select>
+            </label>
+            <br />
+            <label htmlFor="fun">
+              <select
+                name="fun"
+                id="fun"
+                onChange={(e) => setFun(Number(e.target.value))}
+              >
+                <option value={1}>funny</option>
+                <option value={3}>NOT funny</option>
+              </select>
+            </label>
+            <button type="button" onClick={() => setShowCreateNew(false)}>
+              Click me if you change your mind
+            </button>
+          </form>
+        )}
+      </div>
+    </Provider>
   );
 }
 
