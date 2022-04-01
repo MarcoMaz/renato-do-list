31407b890d6088b5c951e62706170f203753faa6
Marco Mazzai <mazzai.marco@gmail.com>
2022-04-01 10:15:42 +0200

    fix( structure ): fix routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,107 +3,36 @@
 // Styling
 import './App.scss';
 
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import { PersistGate } from 'redux-persist/integration/react';
 import { persistStore } from 'redux-persist';
 
 // Redux
 import { Provider } from 'react-redux';
-import { useState } from 'react';
-import { useAppSelector } from './state/hooks';
 import store from './state/store';
 
-// Components
-import Button from './components/core/Button';
-import ThingsTodo from './components/ThingsTodo';
-import ThingsDone from './components/ThingsDone';
-import AddTask from './components/AddTask';
-import Modal from './components/Modal';
-import Toast from './components/Toast';
+// Views
+import LandingView from './views/LandingView';
+import IntroView1 from './views/IntroView1';
+import MainView from './views/MainView';
 
 const persistor = persistStore(store);
 
-function App() {
-  const tasks = useAppSelector((state) => state.task);
-  const completedTodos = useAppSelector((state) =>
-    state.task.map((task) => task.isCompleted),
-  );
-
-  const numberOfThingsTotal = tasks.length;
-  const numberOfThingsDone = completedTodos.filter(Boolean).length;
-  const numberOfThingsTodo = numberOfThingsTotal - numberOfThingsDone;
-
-  const [newItemText, setItemText] = useState('');
-  const [newSpeed, setSpeed] = useState(30);
-  const [newUrgency, setUrgency] = useState(50);
-  const [newFun, setFun] = useState(11);
-
-  const [editIndex, setEditIndex] = useState(0);
-
-  const [showAddTask, setShowAddTask] = useState(false);
-
-  const [modifyTask, setModifyTask] = useState(false);
-
-  const [showModal, setShowModal] = useState(false);
-
-  const [showToast, setShowToast] = useState(false);
-
+const App = () => {
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <div className="App">
-          <ThingsTodo
-            numberOfThingsTodo={numberOfThingsTodo}
-            numberOfThingsTotal={numberOfThingsTotal}
-            tasks={tasks}
-            setSpeed={setSpeed}
-            setUrgency={setUrgency}
-            setFun={setFun}
-            setEditIndex={setEditIndex}
-            setShowAddTask={setShowAddTask}
-            setItemText={setItemText}
-            setModifyTask={setModifyTask}
-            setShowModal={setShowModal}
-          />
-          <ThingsDone
-            numberOfThingsDone={numberOfThingsDone}
-            numberOfThingsTotal={numberOfThingsTotal}
-            tasks={tasks}
-          />
-          {!showAddTask && (
-            <Button
-              type="button"
-              label="+"
-              onClick={() => setShowAddTask(true)}
-            />
-          )}
-          {showAddTask && (
-            <AddTask
-              editIndex={editIndex}
-              itemText={newItemText}
-              speed={newSpeed}
-              urgency={newUrgency}
-              fun={newFun}
-              setItemText={setItemText}
-              setShowCreateNew={setShowAddTask}
-              setSpeed={setSpeed}
-              setUrgency={setUrgency}
-              setFun={setFun}
-              modifyTask={modifyTask}
-              setModifyTask={setModifyTask}
-            />
-          )}
-          {showModal && (
-            <Modal
-              tasks={tasks}
-              setShowModal={setShowModal}
-              setShowToast={setShowToast}
-            />
-          )}
-          {showToast && <Toast />}
+          <Router>
+            <Route exact path="/" component={LandingView} />
+            <Route path="/intro-1" component={IntroView1} />
+            <Route path="/main" component={MainView} />
+          </Router>
         </div>
       </PersistGate>
     </Provider>
   );
-}
+};
 
 export default App;
