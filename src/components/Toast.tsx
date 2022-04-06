758b7638c93b641735a62ea892a611d42233796c
Marco Mazzai <mazzai.marco@gmail.com>
2022-04-06 16:42:17 +0200

    feat( toast ): add styling

--- src/components/Toast.tsx
+++ src/components/Toast.tsx
@@ -4,7 +4,7 @@ import { FunctionComponent } from 'react';
 import copyText from '../assets/copyText';
 
 const Toast: FunctionComponent = () => {
-  return <div className="toast">{copyText.toastLabel}</div>;
+  return <div className="Toast">{copyText.toastLabel}</div>;
 };
 
 export default Toast;
